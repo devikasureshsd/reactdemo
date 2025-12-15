95f430555c91a7a471f8c483739b49a687db015d
Devi <devikasureshsd@gmail.com>
2025-12-15 07:48:34 +0000

    react basics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,133 @@
-import React from"react";
-class App extends React.Component
+function App()
 {
-  render()
+  if(true)
   {
-    return(<>
-    <h1>Welcome to class Components</h1>
-     <h1>Welcome to class Components</h1>
-     <h1>Welcome to class Components</h1>
-      <h1>Welcome to class Components</h1>
-      <p>Sit enim culpa nisi consequat. Magna consequat voluptate deserunt tempor deserunt. Duis elit occaecat ipsum exercitation minim nostrud laborum et sit irure irure qui proident sint.
-
-Aute mollit dolore anim irure. Commodo consequat sint nisi aliqua amet laboris tempor pariatur sint pariatur anim est sit. Tempor proident aute ullamco et occaecat. Sit do irure ea occaecat esse magna reprehenderit commodo nostrud aute ullamco. Anim anim esse incididunt velit ullamco labore eiusmod incididunt quis nostrud. Velit magna elit fugiat duis ut ullamco incididunt.
-
-Culpa Lorem mollit excepteur nostrud. Qui sunt non culpa nisi. Voluptate qui dolore et aliqua. Quis nostrud deserunt mollit Lorem ad ex proident. Excepteur est ut ipsum occaecat consectetur officia laborum in magna cupidatat dolor magna. Anim minim veniam ut duis esse adipisicing culpa consequat velit.
-
-Exercitation esse sint magna veniam reprehenderit occaecat eiusmod velit. Irure sunt mollit ut irure labore nulla elit eiusmod. Ipsum pariatur officia do laborum minim esse Lorem deserunt nostrud sint voluptate tempor. Incididunt labore in reprehenderit eu ullamco commodo anim est incididunt anim consequat aliquip aliqua. Lorem labore exercitation nisi irure aliqua aliquip eu elit commodo voluptate. Ea adipisicing ex ea ex. Ad dolore enim tempor consequat non nostrud aliquip cupidatat laboris aute adipisicing commodo.
-
-Laborum in labore amet magna occaecat incididunt eu exercitation officia. Et veniam adipisicing elit irure mollit sunt laboris labore velit sunt labore. Anim esse id aliquip elit. Amet deserunt consequat do dolore commodo sunt ullamco fugiat nostrud.</p>
-    </>)
+    var v=100
+    var l=200
+    const c=300
+    v++;
+    l++;
+    console.log("let:"+l);
+    // c++
+    console.log("const:"+c);
+    // document.getElementById("res").innerHTML="Let:"+l;
   }
+  return(
+    <>
+    <h1>variable : global : {v}</h1>
+    <h1>does not work let and const : because local</h1>
+    </>
+  )
 }
 export default App
+
+
+
+
+// function App()
+// {
+//   let myfun=(a,b,c)=>{
+//     var tot=a+b+c
+//     document.getElementById("res").innerHTML=`<h2> Totl:${tot}</h2>`; 
+//   // document.getElementById("res").innerHTML="Congrats you clicked the button";
+//   }
+//   return(<>
+//   <h2>Arrow function without parameter</h2>
+//   <h3>Note: onClick C must be caps <br></br>dont't use double quots</h3>
+//   <h3>Note: in calling function don't use parenthesis when without passing args</h3>
+//   <button onClick={()=>myfun(10,20,30)}>click me</button>
+//   <div id="res"></div>
+  
+//   </>)
+// }
+// export default App
+
+
+
+// import myimg from './images/images (3).jpg'
+// function App()
+// {
+//   return(<>
+//   <h2>Hello Pikachu</h2>
+//   <img src={myimg} width="50%" height="auto" alt="path is wrong"></img>
+//   </>)
+// }
+// export default App
+
+
+
+// function App()
+// {
+//   var a=100
+//   var b=500
+//   var c=800
+//   return(
+//     <>
+//     <h2>Biggest Among 2 no's using conditionl or ternaray operator</h2>
+//     <h1>Biggest Number a or b:{a>b?a:b}</h1>
+//     <h1>Biggest Number a or b or c:{(a>b && a>c)?a:(b>c?b:c)}</h1>
+//     </>
+//   )
+// }
+// export default App
+
+
+// function App(){
+//   var a=10
+//   var b=20
+//   var c="Devi Suresh"
+//   return(<>
+//     <h1>Total:{a+b}</h1>
+//     <marquee bgcolor='pink'><font size='7'>{c}</font></marquee>
+//   </>)
+// }
+// export default App
+
+
+
+// import React from"react";
+// class Parent extends React.Component
+// {
+//   render()
+//   {
+//        return(<>
+//        <h1>This is parent class</h1>
+//         <h1>This is parent class</h1>
+//          <h1>This is parent class</h1>
+//          <h1>This is parent class</h1>
+//          <h1>This is parent class</h1>
+//        </>
+//        )
+//   }
+// }
+// class App extends Parent
+// {
+//   render()
+
+//   {
+//     return(<>
+//     <Parent/>
+//     <h1>Welcome to class components</h1>
+//      <h1>Welcome to class components</h1>
+//       <h1>Welcome to class components</h1>
+//        <h1>Welcome to class components</h1>
+//        <hr></hr>
+//        <p>In adipisicing veniam est do et nostrud. Dolor aliquip veniam nulla incididunt commodo voluptate adipisicing dolore id commodo. Duis magna quis amet velit nostrud esse anim id dolore enim et. Sint esse commodo ad do eu. Officia cupidatat velit pariatur ad do tempor ullamco laboris consequat adipisicing ullamco ullamco ut.
+
+// Non tempor eu quis exercitation. Officia anim consequat veniam ipsum dolore nisi veniam fugiat. Cupidatat anim ipsum ad esse minim enim est do irure laboris tempor consectetur elit anim. Sit sunt exercitation aliqua laborum occaecat in labore elit dolore veniam qui adipisicing nostrud. Elit nulla ea elit dolore aute minim labore exercitation.
+
+// Cillum ad sint laboris labore elit ut culpa irure incididunt. Dolor elit voluptate eu Lorem sint ut ad duis voluptate. Laborum qui et ullamco aliquip cupidatat cillum esse do deserunt veniam.
+
+// Sunt qui quis ex adipisicing ad duis et incididunt. Tempor est incididunt qui quis. Amet occaecat nostrud anim voluptate nostrud laboris commodo aliqua dolor sunt. Ad magna velit qui laborum officia sit aliqua. Aute occaecat ad et eu aute fugiat minim do aliquip non laborum. Ad velit laborum sit do dolor commodo in. Minim commodo elit sit minim minim.
+
+// Pariatur adipisicing nostrud sint elit minim enim deserunt officia anim sint. Reprehenderit aliquip enim esse esse elit anim quis officia enim est. Aliqua deserunt velit velit deserunt excepteur ad id nostrud occaecat consequat officia esse veniam. Et officia incididunt esse cupidatat laboris in ipsum consequat consequat. Cillum nisi ullamco dolore officia dolore in.</p>
+//     </>
+
+//     )
+//   }
+// }
+// export default App
+
 
 
 
